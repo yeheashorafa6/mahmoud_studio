b08faa14bb12ca991ee47abf586785760c20374c
Yehea Shorafa <yeheashorafa6@gmail.com>
2024-08-01 15:40:11 +0300

    edit img metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,16 +2,16 @@ import RootLayout from './Layout/RootLayout';
 import ConditionalLayout from './Layout/ConditionalLayout';
 
 export const metadata = {
-
+    metadataBase: new URL("https://mahmoud-studio.vercel.app"),
     title: {
         default: 'mahmoud_studio',
         template: '%s | mahmoud_studio',
       },
-    description: 'مرحبًا بكم في الموقع الشخصي mahmoud_studio، مصمم جرافيك محترف. استمتعوا بمشاهدة أحدث أعمالي المبدعة في تصميم الشعارات، الهويات البصرية، والتصاميم الإعلانية. اكتشفوا إبداعاتي وتعرفوا على الخدمات التي أقدمها لتلبية احتياجاتكم التصميمية بأعلى جودة واحترافية',
+    description: 'مرحبًا بكم في الموقع الشخصي انا محمود الشرفا، مصمم جرافيك محترف. استمتعوا بمشاهدة أحدث أعمالي المبدعة في تصميم الشعارات، الهويات البصرية، والتصاميم الإعلانية. اكتشفوا إبداعاتي وتعرفوا على الخدمات التي أقدمها لتلبية احتياجاتكم التصميمية بأعلى جودة واحترافية',
     openGraph: {
         images: [
           {
-            url: 'https://mahmoud-studio.vercel.app/assets/docLogo.png',
+            url: '/assets/docLogo.png',
             width: 100,
             height: 50,
           },
