a95d5a67dd0f106ed78cc122812dcf67df8e2204
Yehea Shorafa <yeheashorafa6@gmail.com>
2024-08-03 10:39:40 +0300

    edit logo doc

--- src/app/Layout/RootLayout.jsx
+++ src/app/Layout/RootLayout.jsx
@@ -2,20 +2,20 @@ import "./../globals.css";
 import "./../fontStyle.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import DrawingComponent from "@/components/ui/DrawingComponent";
-
-import { Metadata } from 'next';
 import Head from "next/head";
 
-export const getMetadata =   {
 
-    // metadataBase: new URL("https://mahmoud-studio.vercel.app"),
+ const metadata =   {
+
+    metadataBase: new URL("https://mahmoud-studio.vercel.app"),
     title: {
       default: 'mahmoud_studio',
       template: '%s | mahmoud_studio',
     },
-    description: 'مرحبًا بكم في الموقع الشخصي...',
-    link: "/assets/docLogo.ico"
-  
+    description: 'مرحبًا بكم في الموقع الشخصي انا محمود الشرفا، مصمم جرافيك محترف. استمتعوا بمشاهدة أحدث أعمالي المبدعة في تصميم الشعارات، الهويات البصرية، والتصاميم الإعلانية. اكتشفوا إبداعاتي وتعرفوا على الخدمات التي أقدمها لتلبية احتياجاتكم التصميمية بأعلى جودة واحترافية',
+    icons: {
+      icon: '/assets/docLogo.ico'
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -24,10 +24,6 @@ export default function RootLayout({ children }) {
       <body>
         <ThemeProvider attribute="class" defaultTheme="light">
           <DrawingComponent />
-          <Head>
-          <link rel="icon" href="/assets/docLogo.ico" />
-        {/* يمكنك إضافة المزيد من العناصر هنا */}
-      </Head>
           {children}
         </ThemeProvider>
       </body>
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,21 @@
 import RootLayout from './Layout/RootLayout';
 import ConditionalLayout from './Layout/ConditionalLayout';
+import Head from 'next/head';
 
 export const metadata = {
-//     metadataBase: new URL("https://mahmoud-studio.vercel.app"),
     title: {
         default: 'mahmoud_studio',
         template: '%s | mahmoud_studio',
     },
     description: 'مرحبًا بكم في الموقع الشخصي انا محمود الشرفا، مصمم جرافيك محترف. استمتعوا بمشاهدة أحدث أعمالي المبدعة في تصميم الشعارات، الهويات البصرية، والتصاميم الإعلانية. اكتشفوا إبداعاتي وتعرفوا على الخدمات التي أقدمها لتلبية احتياجاتكم التصميمية بأعلى جودة واحترافية',
-
+    icons: {
+      icon: '/assets/docLogo.ico'
+  },
 };
 
 export default function Layout({ children }) {
   return (
+    
     <RootLayout>
       <ConditionalLayout>{children}</ConditionalLayout>
     </RootLayout>
