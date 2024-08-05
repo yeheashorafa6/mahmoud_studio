c7b41adbf3d1bbdb3ea181890c19e67ffb02164b
Yehea Shorafa <yeheashorafa6@gmail.com>
2024-08-05 17:44:59 +0300

    adding

--- src/components/Reviews/ReviewsSec.jsx
+++ src/components/Reviews/ReviewsSec.jsx
@@ -1,16 +1,11 @@
 import { fetchReviwesSec } from '@/lib/data'
-import React, { use } from 'react'
 import Reviews from './Reviews'
 
-async function fetchReviewsData(q, page) {
-    return await fetchReviwesSec(q, page);
-}
-
-function ReviewsSec() {
+async function ReviewsSec() {
     const page = 1;
     const q = "";
-    const review = use(fetchReviewsData(q, page));
-    const reviewData = review ? JSON.parse(JSON.stringify(review)) : "";
+    const review = await fetchReviwesSec(q, page);
+    const reviewData = review ? JSON.parse(JSON.stringify(review)) : [];
 
     return (
         <div>
