1bb93cf5b390d4141cee489809089266717a188b
Yehea Shorafa <yeheashorafa6@gmail.com>
2024-08-07 15:41:36 +0300

    edit motion page

--- src/app/Projects/[_id]/page.jsx
+++ src/app/Projects/[_id]/page.jsx
@@ -3,24 +3,44 @@ import { fetchMotion, fetchProject } from "@/lib/data";
 import ProjectDetalis from "@/components/ProjectDetalis/ProjectDetalis";
 import MotionGraphicsDetalis from "@/components/MotionGraphicsDetalis/MotionGraphicsDetalis";
 
-export async function generateMetadata({ params }) {
-  // افترض أن هذه الدالة تجلب بيانات المشروع من قاعدة البيانات
-  const projectData = await fetchProject(params._id);
-  console.log(projectData)
+export async function generateMetadata({ params, searchParams }) {
+  const { _id } = params;
+  const type = searchParams.type || "";
 
-  return {
-    title: `${projectData.title}`,
-    description: projectData.description,
-    openGraph: {
-      images: [
-        {
-          url: `https://www.mahmoud-studio.com/api/og?title=${encodeURIComponent(projectData.title)}&bg=${encodeURIComponent(projectData.img)}`,
-          width: 1200,
-          height: 630,
-        },
-      ],
-    },
-  };
+  let projectData;
+  let motionData;
+
+  if (type === 'motion') {
+    motionData = await fetchMotion(_id);
+    return {
+      title: `${motionData.title}`,
+      description: motionData.description,
+      openGraph: {
+        images: [
+          {
+            url: `https://www.mahmoud-studio.com/api/og?title=${encodeURIComponent(motionData.title)}&bg=${encodeURIComponent(motionData.img)}`,
+            width: 1200,
+            height: 630,
+          },
+        ],
+      },
+    };
+  } else {
+    projectData = await fetchProject(_id);
+    return {
+      title: `${projectData.title}`,
+      description: projectData.description,
+      openGraph: {
+        images: [
+          {
+            url: `https://www.mahmoud-studio.com/api/og?title=${encodeURIComponent(projectData.title)}&bg=${encodeURIComponent(projectData.img)}`,
+            width: 1200,
+            height: 630,
+          },
+        ],
+      },
+    };
+  }
 }
 async function SingleProjectPage({ params ,searchParams }) {
   const { _id } = params;
