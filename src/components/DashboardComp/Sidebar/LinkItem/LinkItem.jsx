8622ccbffa2410fa4b3016802a15f476906be719
Yehea Shorafa <yeheashorafa6@gmail.com>
2024-08-14 15:57:57 +0300

    edit link sidebar

--- src/components/DashboardComp/Sidebar/LinkItem/LinkItem.jsx
+++ src/components/DashboardComp/Sidebar/LinkItem/LinkItem.jsx
@@ -5,8 +5,12 @@ import React from 'react'
 
 function LinkItem({list}) {
     const path = usePathname();
+    const isActive = 
+        list.path === "/" || list.path === "/Dashboard" 
+            ? path === list.path
+            : path.startsWith(list.path);
   return (
-    <Link href={list.path} className={`${path === list.path && 'bg-[#2e374a]'} flex gap-x-3 items-center hover:bg-[#7d828d] transition-all duration-300 rounded-lg p-5`}>
+    <Link href={list.path} className={`${isActive && 'bg-[#2e374a]'} flex gap-x-3 items-center hover:bg-[#7d828d] transition-all duration-300 rounded-lg p-5`}>
     <span>{list.icon}</span>
     {list.title}
     </Link>
